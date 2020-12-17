d14c68d823fdd96add854b144927668d4f5a6c83
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-17 11:27:55 -0500

    add server and first router

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    console.log('yo world');
+    res.send('yo');
+})
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+});
